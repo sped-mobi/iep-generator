7c098b296cbdb54f1d1672ef6b978e9707080be4
Brad Marshall <bmarshall@sped.mobi>
2021-03-24 02:01:32 -0500

    Add project files.

---
+++ src/Individualized.Education.Plan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Individualized.Education.Plan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/iep-generator/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEP.Generator.Core
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
